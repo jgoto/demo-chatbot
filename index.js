5e8a7ba1fa1f2f827c16eb4a8bd46652c00bfeb7
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2025-03-07 23:42:50 -0500

    Handle interactions and execute interactions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const { token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
@@ -29,5 +29,36 @@ for(const folder of commandFolders){
         else{
             console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
+    }
+}
+
+client.on(Events.InteractionCreate, interaction => {
+    console.log(interaction);
+});
+
+client.on(Events.InteractionCreate, interaction => {
+    if(!interaction.isChatInputCommand()) return;
+    console.log(interaction);
+});
+
+client.on(Events,InteractionCreate, async interaction => {
+    if(!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.command.get(interaction.commandName);
+
+    if(!command) {
+        console.error(`No command matching ${interaction.commandName} was found`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if(interaction.replied || interaction.deferred){
+            await interaction.followUp({ content: 'There was an error while executing this command', flags: MessageFlags.Ephemeral});
+        }
+        else {
+            await interaction.reply({ content: 'There was an error while executing this command', flags: MessageFlags.Ephemeral});
     }
 }
