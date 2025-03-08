3fcd3c766e28e4864b4011343c4c77f2ce1cd118
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2025-03-07 23:22:17 -0500

    Load the commands into a collection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const client = new Client( {Intents: [GatewayIntentBits.Guilds]} );
 
@@ -8,3 +10,24 @@ client.once(Events.ClientReady, readyClient => {
 });
 
 client.login(token);
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for(const folder of commandFolders){
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for(const file of commandFiles){
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the collection with the key as the command name and value as the exported module
+        if('data' in command && 'execute' in command){
+            client.commands.set(command.data.name, command);
+        }
+        else{
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
